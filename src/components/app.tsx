c3c24c7fd18f2073b9846aaea97b27a79494d36e
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-12-01 10:33:02 -0700

    combine app / vis components

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,28 @@
-import { FC } from 'react'
-import Vis from '../components/vis'
+import { FC, useEffect, useRef } from 'react'
+import '../styles/app.css'
 
 const App: FC = () => {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+
+    const resizeCanvas = (): void => {
+        if (!canvasRef.current) {
+            throw new Error('No reference to canvas')
+        }
+        canvasRef.current.width = window.innerWidth * window.devicePixelRatio
+        canvasRef.current.height = window.innerHeight * window.devicePixelRatio
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', resizeCanvas)
+        return () => {
+            window.removeEventListener('resize', resizeCanvas)
+        }
+    }, [])
+
     return (
-        <Vis />
+        <canvas
+            ref={canvasRef}
+        ></canvas>
     )
 }
 
