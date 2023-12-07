220e6c8a4060bf47d018201e0a0580bda1b32aae
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-12-07 11:59:31 -0700

    discard frags outside circle

--- src/shaders/punchcard-frag.glsl
+++ src/shaders/punchcard-frag.glsl
@@ -8,6 +8,9 @@ void main() {
     vec2 cxy = gl_PointCoord * 2.0 - 1.0;
     float radius = dot(cxy, cxy);
     float alpha = 1.0 - smoothstep(0.95, 1.0, radius);
+    if (radius > 1.0) {
+        discard;
+    }
 
     vec4 color = texture2D(mineral, vTexCoord);
     gl_FragColor = color * alpha;
