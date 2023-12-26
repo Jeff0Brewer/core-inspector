b0143bb0826aa9a244378ed06105306d0b11a143
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-12-26 16:36:35 -0700

    ignore underscore unused vars in eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,12 @@ module.exports = {
     rules: {
         '@typescript-eslint/explicit-function-return-type': 'error',
         indent: ['error', 4, { SwitchCase: 1 }],
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_'
+        }]
     }
 }
