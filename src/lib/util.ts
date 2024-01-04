832fdb5ade59bb32cc902047d4a658c9615c841c
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-04 15:00:30 -0700

    add gl-matrix vec types to vec to hex helper

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,5 @@
+import { vec2, vec3, vec4 } from 'gl-matrix'
+
 const ease = (t: number): number => {
     const t2 = t * t
     return t2 / (2 * (t2 - t) + 1)
@@ -7,13 +9,14 @@ const clamp = (v: number, min: number, max: number): number => {
     return Math.max(Math.min(v, max), min)
 }
 
-const vecToHex = (v: Array<number>): string => {
-    return v.map(x => {
-        const hex = x.toString(16)
-        return hex.length === 1
-            ? '0' + hex
-            : hex
-    }).join('')
+const vecToHex = (v: Array<number> | vec2 | vec3 | vec4): string => {
+    const hex = []
+    for (const x of v) {
+        const h = x.toString(16)
+        // add leading 0 if hex value only one char
+        hex.push(h.length === 1 ? '0' + h : h)
+    }
+    return hex.join('')
 }
 
 type BoundRect = {
