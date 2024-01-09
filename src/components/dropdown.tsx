a6bf6498bdae59de70d75c4518a26444e8866a4b
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-09 16:04:15 -0700

    add default item renderer for string values

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,12 +1,32 @@
-import { useState, ReactElement } from 'react'
+import { useState, ReactElement, ReactPortal } from 'react'
 import { PiCaretDownBold } from 'react-icons/pi'
 import '../styles/dropdown.css'
+
+function checkStringType (v: unknown): string {
+    const vType = typeof v
+    if (vType !== 'string') {
+        throw new Error(`Expected type string, got ${vType}`)
+    }
+    return v as 'string'
+}
+
+type StringItemProps<T> = {
+    item: T
+}
+
+function StringItem<T> (
+    { item }: StringItemProps<T>
+): ReactElement {
+    return (
+        <p>{checkStringType(item)}</p>
+    )
+}
 
 type DropdownProps<T> = {
     items: Array<T>,
     selected: T | null,
     setSelected: (s: T) => void,
-    Element: (props: { item: T }) => ReactElement,
+    Element?: (props: { item: T }) => ReactElement,
     customClass?: string
 }
 
@@ -14,6 +34,10 @@ function Dropdown<T> (
     { items, Element, selected, setSelected, customClass }: DropdownProps<T>
 ): ReactElement {
     const [open, setOpen] = useState<boolean>(false)
+
+    if (!Element) {
+        Element = StringItem
+    }
 
     return (
         <div
