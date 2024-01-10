c7f374391d864c9b121cbd40522824e4ae403a8d
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-10 13:03:27 -0700

    style core select dropdown

--- src/components/core-select.tsx
+++ src/components/core-select.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import Dropdown from '../components/dropdown'
+import '../styles/core-select.css'
 
 type CoreSelectProps = {
     cores: Array<string>,
@@ -11,12 +12,13 @@ function CoreSelect (
     { cores, selected, setSelected }: CoreSelectProps
 ): ReactElement {
     return (
-        <div>
+        <div className={'core-select'}>
             <p>core</p>
             <Dropdown
                 items={cores}
                 selected={selected}
                 setSelected={setSelected}
+                customClass={'core-dropdown'}
             />
         </div>
     )
