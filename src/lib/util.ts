1a50f400e79c235612e710077bb3e9980d68fc1f
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-22 18:30:59 -0700

    convert from arrow functions

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,39 +1,39 @@
-import { vec2, vec3, vec4 } from 'gl-matrix'
+import { vec3 } from 'gl-matrix'
 
-const ease = (t: number): number => {
+function ease (t: number): number {
     const t2 = t * t
     return t2 / (2 * (t2 - t) + 1)
 }
 
-const clamp = (v: number, min: number, max: number): number => {
+function clamp (v: number, min: number, max: number): number {
     return Math.max(Math.min(v, max), min)
 }
 
-const byteToHex = (byte: number): string => {
+function byteToHex (byte: number): string {
     const hex = byte.toString(16)
     return hex.length === 1 ? '0' + hex : hex
 }
 
-const bytesToHex = (vec: Array<number>): string => {
+function bytesToHex (vec: Array<number>): string {
     return vec.map(byteToHex).join('')
 }
 
-const floatsToHex = (vec: Array<number>): string => {
+function floatsToHex (vec: Array<number>): string {
     return vec.map(float => {
         const byte = Math.floor(float * 255)
         return byteToHex(byte)
     }).join('')
 }
 
-const formatPercent = (p: number): string => {
+function formatPercent (p: number): string {
     return (p * 100).toFixed() + '%'
 }
 
-const parsePercent = (p: string): number => {
+function parsePercent (p: string): number {
     return parseFloat(p.replace('%', '')) * 0.01
 }
 
-const formatFloat = (f: number): string => {
+function formatFloat (f: number): string {
     return f.toFixed(2)
 }
 
