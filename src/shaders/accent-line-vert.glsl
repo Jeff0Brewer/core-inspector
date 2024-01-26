482150e8ac94a31f553637f1d117762b887c5787
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-26 10:46:54 -0700

    use 0 as breakpoint between bottom / side, add descriptions

--- src/shaders/accent-line-vert.glsl
+++ src/shaders/accent-line-vert.glsl
@@ -12,11 +12,13 @@ varying vec4 vColor;
 void main() {
     vec4 position = spiralPos * shapeT + columnPos * (1.0 - shapeT);
     gl_Position = proj * view * position;
-
     vLineLength = lineLength;
-    vColor = mix(
-        vec4(0.58, 0.46, 0.28, 0.9),
-        vec4(0.34, 0.45, 0.59, 0.9 * (1.0 - shapeT)),
-        smoothstep(1.0, 1.0, lineLength)
-    );
+
+    float isColumn = 1.0 - shapeT;
+    vec4 sideColor = vec4(0.34, 0.45, 0.59, 0.9 * isColumn);
+    vec4 bottomColor = vec4(0.58, 0.46, 0.28, 0.9);
+
+    // switch between colors for side / bottom lines
+    // any vertices with negative lengths are known to be bottom lines
+    vColor = lineLength < 0.0 ? bottomColor : sideColor;
 }
