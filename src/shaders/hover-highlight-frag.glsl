b7d90c6d40ad10e94dc69b2b0cb58b3a09647df1
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-28 18:32:49 -0700

    tune hover gradient

--- src/shaders/hover-highlight-frag.glsl
+++ src/shaders/hover-highlight-frag.glsl
@@ -7,6 +7,6 @@ void main() {
     vec2 dist = mousePos - gl_FragCoord.xy;
     float radius = sqrt(dot(dist, dist)) / windowHeight;
 
-    float gradient = smoothstep(0.0, gl_FragCoord.w * 0.4, radius);
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 0.4 * gradient + 0.05);
+    float gradient = 1.0 - smoothstep(0.0, gl_FragCoord.w * 0.1, radius);
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 0.1 * gradient + 0.08);
 }
