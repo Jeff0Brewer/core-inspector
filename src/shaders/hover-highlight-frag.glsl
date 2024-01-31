d5ed96a100c7d98da0a84e8dfaebb5f183207275
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-01-31 12:02:12 -0700

    increase gradient size

--- src/shaders/hover-highlight-frag.glsl
+++ src/shaders/hover-highlight-frag.glsl
@@ -7,6 +7,6 @@ void main() {
     vec2 dist = mousePos - gl_FragCoord.xy;
     float radius = sqrt(dot(dist, dist)) / windowHeight;
 
-    float gradient = 1.0 - smoothstep(0.0, gl_FragCoord.w * 0.1, radius);
+    float gradient = 1.0 - smoothstep(0.0, gl_FragCoord.w * 0.2, radius);
     gl_FragColor = vec4(1.0, 1.0, 0.0, 0.1 * gradient + 0.08);
 }
