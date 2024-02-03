99a0052ee0807fcf0b94a9a49d2d01efc175c073
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-02-03 15:04:28 -0700

    add transition between single / full view modes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,7 @@
-import { useState, ReactElement } from 'react'
+import { useState, useEffect, ReactElement } from 'react'
 import { COLOR_PRESETS } from '../lib/palettes'
 import FullCore from '../components/full-core'
+import '../styles/app.css'
 
 const CORES = ['gt1', 'gt2', 'gt3']
 const MINERALS = [
@@ -14,21 +15,57 @@ const MINERALS = [
     'carbonate',
     'kaolinite-montmorillinite'
 ]
+const MODE_SWITCH_MS = 500
+const MODE_SWITCH_TRANSITION = `opacity ${MODE_SWITCH_MS}ms ease`
+
+type ViewMode = 'full' | 'single'
 
 function App (): ReactElement {
     const [core, setCore] = useState<string>(CORES[0])
     const [part, setPart] = useState<string | null>(null)
+    const [mode, setMode] = useState<ViewMode>('full')
+    const [render, setRender] = useState<ViewMode | 'both'>(mode)
+
+    useEffect(() => {
+        setRender('both')
+        if (part) {
+            setMode('single')
+            window.setTimeout(() => setRender('single'), MODE_SWITCH_MS)
+        } else {
+            setMode('full')
+            window.setTimeout(() => setRender('full'), MODE_SWITCH_MS)
+        }
+    }, [part])
 
     return (
         <main>
-            { !part && <FullCore
-                cores={CORES}
-                minerals={MINERALS}
-                palettes={COLOR_PRESETS}
-                core={core}
-                setCore={setCore}
-                setPart={setPart}
-            /> }
+            <section
+                className={'full-view'}
+                style={{
+                    transition: MODE_SWITCH_TRANSITION,
+                    opacity: mode === 'full' ? 1 : 0
+                }}
+            >
+                { render !== 'single' && <FullCore
+                    cores={CORES}
+                    minerals={MINERALS}
+                    palettes={COLOR_PRESETS}
+                    core={core}
+                    setCore={setCore}
+                    setPart={setPart}
+                /> }
+            </section>
+            <section
+                className={'single-view'}
+                style={{
+                    transition: MODE_SWITCH_TRANSITION,
+                    opacity: mode === 'single' ? 1 : 0
+                }}
+            >
+                { render !== 'full' && <p>
+                    single view
+                </p> }
+            </section>
         </main>
     )
 }
