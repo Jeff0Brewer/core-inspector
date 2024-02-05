3a8b35de0adc28f6b0094c4d9139947c0a0b62f1
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-02-05 11:48:28 -0700

    remove transition between views

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -17,56 +17,29 @@ const MINERALS = [
     'kaolinite-montmorillinite'
 ]
 
-type ViewMode = 'single' | 'full'
-
 function App (): ReactElement {
-    const [mode, setMode] = useState<ViewMode>('full')
     const [core, setCore] = useState<string>(CORES[0])
     const [part, setPart] = useState<string | null>(null)
-    const [transitioning, setTransitioning] = useState<boolean>(false)
-
-    const startTransition = (): void => {
-        setTransitioning(true)
-        window.setTimeout(() => setTransitioning(false), 1000)
-    }
-
-    const viewSinglePart = (part: string): void => {
-        setPart(part)
-        startTransition()
-        window.setTimeout(() => setMode('single'), 50)
-    }
-
-    const viewFullCore = (): void => {
-        setPart(null)
-        startTransition()
-        window.setTimeout(() => setMode('full'), 50)
-    }
 
     return (
         <main>
-            { (mode === 'full' || transitioning) && <div
-                className={'full-view-wrap'}
-                data-visible={mode === 'full'}
-            >
+            { part === null && <div className={'full-view-wrap'}>
                 <FullCore
                     cores={CORES}
                     minerals={MINERALS}
                     palettes={COLOR_PRESETS}
                     core={core}
                     setCore={setCore}
-                    setPart={viewSinglePart}
+                    setPart={setPart}
                 />
             </div> }
-            { (mode === 'single' || transitioning) && <div
-                className={'single-view-wrap'}
-                data-visible={mode === 'single'}
-            >
+            { part !== null && <div className={'single-view-wrap'}>
                 <SinglePart
                     part={part}
                     core={core}
                     minerals={MINERALS}
                     palettes={COLOR_PRESETS}
-                    clearPart={viewFullCore}
+                    clearPart={() => setPart(null)}
                 />
             </div> }
         </main>
