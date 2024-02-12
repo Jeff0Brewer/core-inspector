8ed098b8377379d3e92967d6f875e360e7401119
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-02-12 14:27:09 -0700

    add top depth / length / hydration to part info header

--- src/components/part/info-header.tsx
+++ src/components/part/info-header.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react'
+import { useCoreMetadata } from '../../hooks/core-metadata-context'
 import { getCoreId, getPartId } from '../../lib/ids'
+import { formatFloat } from '../../lib/util'
 
 type PartInfoHeaderProps = {
     core: string,
@@ -9,11 +11,44 @@ type PartInfoHeaderProps = {
 function PartInfoHeader (
     { core, part }: PartInfoHeaderProps
 ): ReactElement {
+    const { depths, hydrations } = useCoreMetadata()
+
     return (
         <div className={'top'}>
             <div className={'section-info'}>
-                <p> core <span>{ getCoreId(core) }</span> </p>
-                <p> section <span>{ getPartId(part) }</span> </p>
+                <p>
+                    core
+                    <span>
+                        {getCoreId(core)}
+                    </span>
+                </p>
+                <p>
+                    section
+                    <span>
+                        {getPartId(part)}
+                    </span>
+                </p>
+                { !!depths[part] && <>
+                    <p>
+                        top depth
+                        <span className={'lower'}>
+                            {formatFloat(depths[part].topDepth)}m
+                        </span>
+                    </p>
+                    <p>
+                        length
+                        <span className={'lower'}>
+                            {formatFloat(depths[part].length)}m
+                        </span>
+                    </p>
+                </> }
+                { !!hydrations[part] &&
+                    <p>
+                        hydration
+                        <span>
+                            {formatFloat(hydrations[part] * 100)}%
+                        </span>
+                    </p> }
             </div>
         </div>
     )
