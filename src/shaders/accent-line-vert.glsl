033e085d0b53466feefee45690b15fbc24b96585
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-02-20 00:17:38 -0700

    align single / full core accent line colors

--- src/shaders/accent-line-vert.glsl
+++ src/shaders/accent-line-vert.glsl
@@ -15,8 +15,8 @@ void main() {
     vLineLength = lineLength;
 
     float isColumn = 1.0 - shapeT;
-    vec4 sideColor = vec4(0.4, 0.5, 0.65, 1.0 * isColumn);
-    vec4 bottomColor = vec4(0.6, 0.5, 0.3, 1.0);
+    vec4 sideColor = vec4(0.8745, 0.8313, 0.8824, 0.6 * isColumn);
+    vec4 bottomColor = vec4(0.8235, 0.8667, 0.7725, 1.0);
 
     // switch between colors for side / bottom lines
     // any vertices with negative lengths are known to be bottom lines
