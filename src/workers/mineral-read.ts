d4fb1e0a15728838fc585d000fc1f443e5f8439d
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-03-06 14:14:30 -0700

    convert rgba imagedata to monochrome bytes in worker

--- src/workers/mineral-read.ts
+++ src/workers/mineral-read.ts
@@ -1,21 +1,41 @@
 import { StringMap } from '../lib/util'
 
-let imgData: StringMap<ImageData> = {}
-let imgWidth: number = 0
+let pixels: StringMap<Uint8Array> = {}
+let width: number = 0
 
 onmessage = ({ data }): void => {
     if (data.type === 'imgData') {
-        imgData = data.imgData
-        imgWidth = data.imgWidth
+        const imgData: StringMap<ImageData> = data.imgData
+        const minerals = Object.keys(imgData)
+        const monochromeData = Object.values(imgData).map(
+            imgData => imageDataToMonochromeBytes(imgData)
+        )
+        pixels = {}
+        minerals.forEach((mineral, i) => {
+            pixels[mineral] = monochromeData[i]
+        })
+        width = data.imgWidth
     } else if (data.type === 'mousePosition') {
         const { x, y } = data
-        const rowIndex = Math.round(x) * 4
-        const colIndex = Math.round(y) * 4
-        const index = rowIndex + colIndex * imgWidth
+        const rowIndex = Math.round(x)
+        const colIndex = Math.round(y)
+        const index = rowIndex + colIndex * width
         const abundances: StringMap<number> = {}
-        Object.entries(imgData).forEach(([mineral, imgData]) => {
-            abundances[mineral] = imgData.data[index]
+        Object.entries(pixels).forEach(([mineral, data]) => {
+            abundances[mineral] = data[index]
         })
         postMessage({ abundances })
     }
+}
+
+function imageDataToMonochromeBytes (imageData: ImageData): Uint8Array {
+    const { data } = imageData
+    const bytes = new Uint8Array(data.length / 4)
+    for (let i = 0; i < bytes.length; i++) {
+        const r = data[i * 4]
+        const g = data[i * 4 + 1]
+        const b = data[i * 4 + 2]
+        bytes[i] = (r + g + b) * 0.333333
+    }
+    return bytes
 }
