c3cac29fd4342753c49c5da23ce06ce74162e2c3
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-03-24 14:13:21 -0600

    remove scale ruler, tune style / labels

--- src/components/part/view-controls.tsx
+++ src/components/part/view-controls.tsx
@@ -1,37 +1,21 @@
-import { useState, useEffect, ReactElement } from 'react'
+import { ReactElement } from 'react'
 import { PiArrowsHorizontalBold } from 'react-icons/pi'
 import { IoSearch } from 'react-icons/io5'
-import { useCoreMetadata } from '../../hooks/core-metadata-context'
 import VerticalSlider from '../../components/generic/vertical-slider'
 import styles from '../../styles/part/view-controls.module.css'
 
 type PartViewControlsProps = {
-    part: string,
     zoom: number,
-    setZoom: (z: number) => void,
     spacing: number,
-    setSpacing: (s: number) => void,
-    channelHeight: number
+    setZoom: (z: number) => void,
+    setSpacing: (s: number) => void
 }
 
 function PartViewControls (
-    { part, zoom, setZoom, spacing, setSpacing, channelHeight }: PartViewControlsProps
+    { zoom, spacing, setZoom, setSpacing }: PartViewControlsProps
 ): ReactElement {
     return (
         <div className={styles.viewControls}>
-            <ScaleRuler
-                part={part}
-                channelHeight={channelHeight}
-            />
-            <VerticalSlider
-                value={zoom}
-                setValue={setZoom}
-                label={'zoom'}
-                icon={ICONS.zoom}
-                min={0}
-                max={1}
-                step={0.01}
-            />
             <VerticalSlider
                 value={spacing}
                 setValue={setSpacing}
@@ -41,33 +25,15 @@ function PartViewControls (
                 max={1}
                 step={0.01}
             />
-        </div>
-    )
-}
-type ScaleRulerProps = {
-    part: string,
-    channelHeight: number
-}
-
-function ScaleRuler (
-    { part, channelHeight }: ScaleRulerProps
-): ReactElement {
-    const { depths } = useCoreMetadata()
-    const [scale, setScale] = useState<number>(0)
-
-    useEffect(() => {
-        if (!depths || !channelHeight) { return }
-        const cmPerPx = (depths[part].length * 100) / channelHeight
-        setScale(cmPerPx * 75)
-    }, [part, depths, channelHeight])
-
-    return (
-        <div className={styles.scaleRuler}>
-            <div className={styles.scaleRulerCenter}>
-                <p>{scale.toFixed(1)} cm</p>
-                <div></div>
-                <p>75 px</p>
-            </div>
+            <VerticalSlider
+                value={zoom}
+                setValue={setZoom}
+                label={'zoom single track'}
+                icon={ICONS.zoom}
+                min={0}
+                max={1}
+                step={0.01}
+            />
         </div>
     )
 }
