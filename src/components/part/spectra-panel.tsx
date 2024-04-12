c1ed70da240659a98a5e66ef0aad92300ae29de7
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-04-12 13:50:52 -0600

    init chart js spectra plot

--- src/components/part/spectra-panel.tsx
+++ src/components/part/spectra-panel.tsx
@@ -2,6 +2,79 @@ import { ReactElement, useState, useEffect } from 'react'
 import { PiCaretRightBold } from 'react-icons/pi'
 import styles from '../../styles/part/spectra-panel.module.css'
 
+import { Line } from 'react-chartjs-2'
+import { Chart, LinearScale, LineElement, PointElement, Filler, Tooltip, ChartOptions } from 'chart.js'
+Chart.register(LinearScale, LineElement, PointElement, Filler, Tooltip)
+Chart.defaults.color = '#ccc'
+Chart.defaults.font.family = 'system-ui'
+const CHART_OPTIONS: ChartOptions<'line'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        tooltip: {
+            mode: 'x',
+            intersect: false,
+            displayColors: false
+        }
+    },
+    elements: {
+        point: {
+            radius: 0,
+            hoverRadius: 0
+        }
+    },
+    scales: {
+        x: {
+            type: 'linear',
+            title: {
+                display: true,
+                text: 'WAVELENGTH',
+                font: {
+                    size: 14,
+                    weight: 200
+                }
+            },
+            border: {
+                color: '#fff'
+            },
+            grid: {
+                tickColor: '#ccc',
+                color: 'transparent'
+            },
+            ticks: {
+                font: {
+                    size: 10
+                }
+            },
+            bounds: 'data'
+        },
+        y: {
+            type: 'linear',
+            title: {
+                display: true,
+                text: 'REFLECTANCE',
+                font: {
+                    size: 14,
+                    weight: 200
+                }
+            },
+            border: {
+                color: '#fff'
+            },
+            grid: {
+                tickColor: '#ccc',
+                color: 'transparent'
+            },
+            ticks: {
+                font: {
+                    size: 10
+                }
+            },
+            bounds: 'data'
+        }
+    }
+}
+
 type SpectraPanelProps = {
     spectra: Array<number>
 }
@@ -24,9 +97,41 @@ function SpectraPanel (
                 >
                     <PiCaretRightBold />
                 </button>
+                <div className={styles.mainPlot}>
+                    <Line
+                        data={{
+                            datasets: [{
+                                data: convData(
+                                    spectra.map((_, i) => i),
+                                    spectra
+                                ),
+                                borderColor: '#fff',
+                                borderWidth: 2,
+                                backgroundColor: 'rgba(255, 255, 255, 0.35)',
+                                fill: 'stack'
+                            }]
+                        }}
+                        options={CHART_OPTIONS}
+                    />
+                </div>
             </div>
         </div>
     )
+}
+
+function convData (x: Array<number>, y: Array<number>): Array<{x: number, y: number}> {
+    if (x.length !== y.length) {
+        throw new Error('Different num of x and y values')
+    }
+    const data: Array<{x: number, y: number}> = []
+    for (let i = 0; i < x.length; i++) {
+        data.push({
+            x: x[i],
+            y: y[i]
+        })
+    }
+
+    return data
 }
 
 export default SpectraPanel
