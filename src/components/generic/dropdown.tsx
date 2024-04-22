74b96b100327d80a0420bbf938faa22774f593c3
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-04-22 13:15:41 -0600

    prevent dropdown items from overflowing below window

--- src/components/generic/dropdown.tsx
+++ src/components/generic/dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState, ReactElement } from 'react'
+import { useState, useRef, useEffect, ReactElement } from 'react'
 import { PiCaretDownBold } from 'react-icons/pi'
 import { checkStringType, StringMap } from '../../lib/util'
 import styles from '../../styles/generic/dropdown.module.css'
@@ -32,6 +32,27 @@ function Dropdown<T> ({
     Element = StringItem
 }: DropdownProps<T>): ReactElement {
     const [open, setOpen] = useState<boolean>(false)
+    const itemsRef = useRef<HTMLDivElement>(null)
+
+    // prevent dropdown items from overflowing past window
+    useEffect(() => {
+        const preventItemsOverflow = (): void => {
+            if (!open || !itemsRef.current) { return }
+            const { top, bottom } = itemsRef.current.getBoundingClientRect()
+            const maxBottom = window.innerHeight - 15
+            if (bottom > maxBottom) {
+                itemsRef.current.style.maxHeight = `${maxBottom - top}px`
+            } else {
+                itemsRef.current.style.removeProperty('max-height')
+            }
+        }
+        preventItemsOverflow()
+
+        window.addEventListener('resize', preventItemsOverflow)
+        return () => {
+            window.removeEventListener('resize', preventItemsOverflow)
+        }
+    }, [open])
 
     return (
         <div
@@ -50,7 +71,10 @@ function Dropdown<T> ({
                     <PiCaretDownBold />
                 </button>
             </div>
-            <div className={`${styles.dropdownItems} ${customStyles?.dropdownItems}`}>
+            <div
+                className={`${styles.dropdownItems} ${customStyles?.dropdownItems}`}
+                ref={itemsRef}
+            >
                 { items.map((item, i) =>
                     <a key={i} onClick={() => {
                         setSelected(item)
