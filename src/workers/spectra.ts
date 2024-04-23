c593a3dbe669e056f072cce352ef61974f8edae3
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-04-23 12:00:37 -0600

    move spectra path helpers to path file, simplify spectra worker

--- src/workers/spectra.ts
+++ src/workers/spectra.ts
@@ -1,9 +1,10 @@
 import { TextWriter, BlobReader, ZipReader } from '@zip.js/zip.js'
-import { padZeros, clamp, StringMap } from '../lib/util'
-import { getSpectraPath } from '../lib/path'
+import { clamp, StringMap } from '../lib/util'
+import { getSpectraBasePath, getSpectraSlicesId } from '../lib/path'
 
 type TypedArray = Uint8Array | Uint16Array
 
+// fetched data format
 type SpectraData = {
     width: number,
     nsamples: number,
@@ -18,9 +19,18 @@ type SpectraData = {
     end_slice: number,
     min_value: number,
     max_value: number,
-    data: string
+    data: string // base64 string encoding bytes or shorts
 }
 
+// format of spectra files, contains extension of files and
+// helpers for base64 conversion
+type Base64Format = {
+    fileExtension: string,
+    fromBase64: (b64: string) => TypedArray,
+    toFloat: (v: number) => number
+}
+
+// converted data, chunk of spectra represented as typed array
 type SpectraChunk = {
     width: number,
     height: number,
@@ -30,45 +40,16 @@ type SpectraChunk = {
     data: TypedArray
 }
 
-type Base64Parser = {
-    fileExtension: string,
-    fromBase64: (b64: string) => TypedArray,
-    toFloat: (v: number) => number
-}
-
-const BYTE_PARSER: Base64Parser = {
+const BYTE_FORMAT: Base64Format = {
     fileExtension: 'HWS.byte-b64.json.zip',
     fromBase64: base64ToU8,
     toFloat: v => v / 255
 }
 
-const SHORT_PARSER: Base64Parser = {
+const SHORT_FORMAT: Base64Format = {
     fileExtension: 'HWS.short-b64.json.zip',
     fromBase64: base64ToU16,
     toFloat: v => v / 65535
-}
-
-const PATH_ROOT = import.meta.env.MODE === 'production' ? '..' : '../..'
-const CLICK_PARSER = import.meta.env.MODE === 'production' ? SHORT_PARSER : BYTE_PARSER
-const HOVER_PARSER = BYTE_PARSER
-
-const REDUCE_FACTOR = 4
-const SLICE_COUNT = 16
-const SPECTRA_TYPE = `W${REDUCE_FACTOR}_S1_H${REDUCE_FACTOR}-n${SLICE_COUNT}`
-
-function getSpectraBasePath (core: string, part: string, root: string): string {
-    const dir = getSpectraPath(core, part, SPECTRA_TYPE, root)
-
-    const [section, piece] = part.split('_').map(s => parseInt(s))
-    const file = `${core.toUpperCase()}A_${section}Z-${piece}_${SPECTRA_TYPE}`
-
-    return `${dir}/${file}`
-}
-
-function getSlicesPath (sliceInd: number, imgHeight: number): string {
-    const minSlice = sliceInd - (sliceInd % SLICE_COUNT)
-    const maxSlice = Math.min(minSlice + SLICE_COUNT, imgHeight) - 1
-    return `${padZeros(minSlice, 4)}-${padZeros(maxSlice, 4)}`
 }
 
 function base64ToU8 (base64: string): Uint8Array {
@@ -85,7 +66,17 @@ function base64ToU16 (base64: string): Uint16Array {
     return new Uint16Array(bytes.buffer)
 }
 
-async function getZipText (path: string): Promise<string> {
+const PATH_ROOT = import.meta.env.MODE === 'production' ? '..' : '../..'
+const CLICK_PARSER = import.meta.env.MODE === 'production' ? SHORT_FORMAT : BYTE_FORMAT
+const HOVER_PARSER = BYTE_FORMAT
+const REDUCE_FACTOR = 4
+const SLICE_COUNT = 16
+const SPECTRA_TYPE = `W${REDUCE_FACTOR}_S1_H${REDUCE_FACTOR}-n${SLICE_COUNT}`
+
+async function getChunk (
+    path: string,
+    parse: (b64: string) => TypedArray
+): Promise<SpectraChunk> {
     const res = await fetch(path)
     const blob = await res.blob()
 
@@ -99,14 +90,10 @@ async function getZipText (path: string): Promise<string> {
     }
 
     const textWriter = new TextWriter()
-    const text = await firstEntry.getData(textWriter)
+    const jsonString = await firstEntry.getData(textWriter)
+
     zipReader.close()
 
-    return text
-}
-
-async function getChunk (path: string, parse: (b64: string) => TypedArray): Promise<SpectraChunk> {
-    const jsonString = await getZipText(path)
     const data: SpectraData = JSON.parse(jsonString)
     return {
         width: data.width_reduced,
@@ -118,22 +105,37 @@ async function getChunk (path: string, parse: (b64: string) => TypedArray): Prom
     }
 }
 
-function getSpectrum (x: number, y: number, chunk: SpectraChunk, parser: Base64Parser): Array<number> {
+function getSpectrum (
+    x: number,
+    y: number,
+    chunk: SpectraChunk,
+    toFloat: (v: number) => number
+): Array<number> {
     const { startSlice, data, width, height, samples } = chunk
-    const rowIndex = clamp(Math.round(x / REDUCE_FACTOR), 0, width - 1)
-    const colIndex = clamp(Math.round((y - startSlice) / REDUCE_FACTOR), 0, height - 1)
+
+    let colIndex = Math.round((y - startSlice) / REDUCE_FACTOR)
+    colIndex = clamp(colIndex, 0, height - 1)
+
+    let rowIndex = Math.round(x / REDUCE_FACTOR)
+    rowIndex = clamp(rowIndex, 0, width - 1)
+
     const startIndex = (colIndex * width + rowIndex) * samples
     const spectrumTyped = data.slice(startIndex, startIndex + samples)
 
     // convert to float array in range (0, 1)
-    return [...spectrumTyped].map(parser.toFloat)
+    return [...spectrumTyped].map(toFloat)
 }
 
-async function getClickedSpectrum (x: number, y: number, slicePath: string, parser: Base64Parser): Promise<void> {
+async function getClickedSpectrum (
+    x: number,
+    y: number,
+    slicePath: string,
+    parser: Base64Format
+): Promise<void> {
     const path = `${slicePath}.${parser.fileExtension}`
 
     const chunk = await getChunk(path, parser.fromBase64)
-    const spectrum = getSpectrum(x, y, chunk, parser)
+    const spectrum = getSpectrum(x, y, chunk, parser.toFloat)
 
     postMessage({
         type: 'clicked',
@@ -143,12 +145,17 @@ async function getClickedSpectrum (x: number, y: number, slicePath: string, pars
     })
 }
 
-function getHoveredSpectrum (x: number, y: number, slicePath: string, parser: Base64Parser): void {
+function getHoveredSpectrum (
+    x: number,
+    y: number,
+    slicePath: string,
+    parser: Base64Format
+): void {
     const path = `${slicePath}.${parser.fileExtension}`
 
     const chunk = sliceCache[path]
     if (chunk) {
-        const spectrum = getSpectrum(x, y, chunk, parser)
+        const spectrum = getSpectrum(x, y, chunk, parser.toFloat)
         postMessage({
             type: 'hovered',
             spectrum
@@ -162,12 +169,13 @@ function getHoveredSpectrum (x: number, y: number, slicePath: string, parser: Ba
     }
 }
 
+// TODO: send back spectrum of mouse position on chunk load
+let sliceCache: StringMap<SpectraChunk> = {}
 const cacheSlices = async (path: string): Promise<void> => {
     const chunk = await getChunk(path, base64ToU8)
     sliceCache[path] = chunk
 }
 
-let sliceCache: StringMap<SpectraChunk> = {}
 let core = ''
 let part = ''
 let imgHeight = 0
@@ -180,7 +188,8 @@ onmessage = ({ data }): void => {
     if (data.type === 'mousePosition') {
         x = data.x
         y = data.y
-        slicePath = `${basePath}.${getSlicesPath(Math.round(y), imgHeight)}`
+        const slicesId = getSpectraSlicesId(imgHeight, Math.round(y), SLICE_COUNT)
+        slicePath = `${basePath}.${slicesId}`
         getHoveredSpectrum(x, y, slicePath, HOVER_PARSER)
     } else if (data.type === 'mouseClick') {
         getClickedSpectrum(x, y, slicePath, CLICK_PARSER)
@@ -189,6 +198,6 @@ onmessage = ({ data }): void => {
         part = data.part
         imgHeight = data.imgHeight
         sliceCache = {}
-        basePath = getSpectraBasePath(core, part, PATH_ROOT)
+        basePath = getSpectraBasePath(core, part, SPECTRA_TYPE, PATH_ROOT)
     }
 }
