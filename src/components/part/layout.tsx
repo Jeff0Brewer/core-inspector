6d0e52de5beaf8c2d15e31cf66e2eb8f39f19eb4
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-04-29 17:09:08 -0600

    add label to part view back button

--- src/components/part/layout.tsx
+++ src/components/part/layout.tsx
@@ -131,6 +131,9 @@ const PartView = React.memo((
     return (
         <div className={styles.partView} style={gridParams}>
             <div className={styles.topLeft}>
+                <p className={`${styles.closeLabel} ${!corePanelOpen && styles.closeLabelHidden}`}>
+                    back to global view
+                </p>
                 <button
                     className={styles.closeButton}
                     onClick={() => setPart(null)}
