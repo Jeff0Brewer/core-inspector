8a54bcf3e91d29ca301eb84f824c010e949ec261
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-04-29 17:12:13 -0600

    open bug form in new tab

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ function Logo (): ReactElement {
     return (
 
         <div className={styles.title}>
-            <a className={styles.bugIcon} href={'https://bit.ly/COREINSPECTOR-BUGS'}>
+            <a className={styles.bugIcon} target={'_blank'} href={'https://bit.ly/COREINSPECTOR-BUGS'}>
                 <img src={BugSvg} />
             </a>
             <a className={styles.logo} href={'https://coreinspector.caltech.edu/HOME.html'}>
