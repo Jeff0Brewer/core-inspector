378acaa630ac78acfec191981958a6f66edfc7e7
Jeff Brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2025-12-17 19:14:56 -0700

    improve error messages

--- src/lib/load.ts
+++ src/lib/load.ts
@@ -2,7 +2,7 @@ function fetchJson<T> (path: string): Promise<T | null> {
     return fetch(path)
         .then(res => res.json())
         .catch(err => {
-            console.error(err)
+            console.error(`Couldn't load path ${path} ${err}`)
             return null
         })
 }
@@ -11,21 +11,21 @@ function fetchBlob (path: string): Promise<Blob | null> {
     return fetch(path)
         .then(res => res.blob())
         .catch(err => {
-            console.error(err)
+            console.error(`Couldn't load path ${path} ${err}`)
             return null
         })
 }
 
 // wrap image load event in promise for async use
-async function loadImageAsync (source: string): Promise<HTMLImageElement | null> {
+async function loadImageAsync (path: string): Promise<HTMLImageElement | null> {
     return new Promise((resolve, _reject) => {
         const image = new Image()
-        image.src = source
+        image.src = path
         image.addEventListener('load', (): void => {
             resolve(image)
         })
         image.addEventListener('error', (err): void => {
-            console.error(err)
+            console.error(`Couldn't load path ${path} ${err}`)
             resolve(null)
         })
     })
